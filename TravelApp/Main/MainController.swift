156738b6a06f6192067e77e6e970c1c4b9068ecd
Daniel Berger <doniberger@gmail.com>
2020-01-15 16:07:29 -0500

    third commit

--- TravelApp/Main/MainController.swift
+++ TravelApp/Main/MainController.swift
@@ -10,6 +10,18 @@ import UIKit
 import MapKit
 import LBTATools
 
+extension MainController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        var annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "id")
+        annotationView.canShowCallout = true
+        annotationView.image = UIImage(imageLiteralResourceName: "tourist")
+        
+        return annotationView
+    }
+}
+
 class MainController: UIViewController {
     
      let mkMapView = MKMapView()
@@ -17,11 +29,14 @@ class MainController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        mkMapView.delegate = self
         view.addSubview(mkMapView)
         mkMapView.fillSuperview()
         mkMapView.mapType = .standard
         
         setupRegionForMap()
+        setupAnnotaionsForMap()
+        
 //        mkMapView.translatesAutoresizingMaskIntoConstraints = false
 //        mkMapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
 //        mkMapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
@@ -40,6 +55,25 @@ class MainController: UIViewController {
         let region = MKCoordinateRegion(center: manhattanCoordinate, span: coordinateSpan)
         
         mkMapView.setRegion(region, animated: true)
+    }
+    
+    fileprivate func setupAnnotaionsForMap() {
+     
+        let nycAnnotation = MKPointAnnotation()
+        nycAnnotation.coordinate = CLLocationCoordinate2D(latitude: 40.765671, longitude: -73.974302)
+        nycAnnotation.title = "New York City :)"
+        nycAnnotation.subtitle = "New York"
+        mkMapView.addAnnotation(nycAnnotation)
+        
+        let homeAnnotation = MKPointAnnotation()
+        homeAnnotation.coordinate = .init(latitude: 41.189428, longitude: -74.053212)
+        homeAnnotation.title = "Home Sweet Home"
+        homeAnnotation.subtitle = "Pomona, NY"
+        mkMapView.addAnnotation(homeAnnotation)
+        mkMapView.showAnnotations(self.mkMapView.annotations, animated: true)
+        mkMapView.showsScale = true
+        mkMapView.showsCompass = true
+       
     }
     
 }
