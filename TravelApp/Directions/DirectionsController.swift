7b6259942b6206cdcb4f1ff199c37d955551829e
Daniel Berger <doniberger@gmail.com>
2020-01-25 23:10:07 -0500

    commit 12

--- TravelApp/Directions/DirectionsController.swift
+++ TravelApp/Directions/DirectionsController.swift
@@ -11,6 +11,16 @@ import SwiftUI
 import MapKit
 import LBTATools
 
+extension DirectionsController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+        polylineRenderer.strokeColor = #colorLiteral(red: 0.1247136965, green: 0.5529010892, blue: 0.950386107, alpha: 1)
+        polylineRenderer.lineWidth = 5
+        return polylineRenderer
+    }
+}
+
 class DirectionsController: UIViewController {
     
     let mapView = MKMapView()
@@ -19,6 +29,7 @@ class DirectionsController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        mapView.delegate = self
         view.addSubview(mapView)
         
         setupRegionForMap()
@@ -31,6 +42,45 @@ class DirectionsController: UIViewController {
         mapView.showsUserLocation = true
 
         setupStartEndDummyAnnotation()
+        requestForDirections()
+    }
+    
+    fileprivate func requestForDirections() {
+        let request = MKDirections.Request()
+
+        let startingPlacemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 40.753402, longitude: -73.982271))
+        request.source = MKMapItem(placemark: startingPlacemark)
+        
+        let endingPlacemark = MKPlacemark(coordinate: CLLocationCoordinate2D(latitude: 40.706175, longitude: -74.018171))
+        request.destination = MKMapItem(placemark: endingPlacemark)
+        
+//        request.requestsAlternateRoutes
+//        request.transportType = MKDirectionsTransportType.walking
+        request.transportType = MKDirectionsTransportType.automobile
+        request.requestsAlternateRoutes = true
+        
+        let directions = MKDirections(request: request)
+        directions.calculate { (response, error) in
+            if let error = error {
+                print("Failed to calculate direction ", error)
+                return
+            }
+            
+            print("Found directions routing...")
+//            guard let route = response?.routes.first else { return }
+//            print("route.steps: ",route.steps)
+//            print("route.expectedTravelTime: ",route.expectedTravelTime) // in seconds
+//            print("Travel time is: ",(route.expectedTravelTime / 60 / 60))
+//            print("route.advisoryNotices: ",route.advisoryNotices)
+//            print("route.polyline: ",route.polyline)
+
+            // show all possible routes
+            response?.routes.forEach({ (route) in
+                self.mapView.addOverlay(route.polyline)
+            })
+            
+//            self.mapView.addOverlay(route.polyline)
+        }
     }
     
     fileprivate func setupStartEndDummyAnnotation() {
@@ -49,7 +99,7 @@ class DirectionsController: UIViewController {
     }
     
     fileprivate func setupNavBar() {
-        navBar.backgroundColor = #colorLiteral(red: 0.1259145737, green: 0.5621746778, blue: 0.9666383862, alpha: 1)
+        navBar.backgroundColor = #colorLiteral(red: 0.1259145737, green: 0.5621746778, blue: 0.9666383862, alpha: 1) //colorLiteral
         view.addSubview(navBar)
         navBar.setupShadow(opacity: 0.5, radius: 5)
         navBar.anchor(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.topAnchor, trailing: view.trailingAnchor, padding: .init(top: 0, left: 0, bottom: -100, right: 0))
